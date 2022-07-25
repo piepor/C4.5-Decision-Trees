22e4d637907a5a50c34178f1fc19fa1b752bca1d
piepor <piepor@users.noreply.github.com>
2022-07-25 13:42:05 +0200

    add decision tree attributes and main class node attributes

--- attributes.py
+++ attributes.py
@@ -1,9 +1,17 @@
 """ Defines a class containing information about the node attributes needed in the init """
 from __future__ import annotations
-from enum import Enum
+from enum import Enum, auto
 from dataclasses import dataclass
 from typing import Callable
 from leaf_classes import LeafClasses
+
+
+class NodeType(Enum):
+    """ types of node in the decision tree """
+    DECISION_NODE_CONTINUOUS = auto()
+    DECISION_NODE_CATEGORICAL = auto()
+    DECISION_NODE_BOOLEAN = auto()
+    LEAF_NODE = auto()
 
 
 class AttributeType(Enum):
@@ -14,17 +22,45 @@ class AttributeType(Enum):
 
 
 @dataclass
-class DecisionNodeAttributes:
-    """ attributes class """
+class NodeAttributes:
+    """ general node attributes """
     node_level: int
     node_name: str
+    node_type: NodeType
+
+
+@dataclass
+class DecisionNodeAttributes(NodeAttributes):
+    """ decision node attributes class """
     attribute_name: str
     attribute_type: AttributeType
     threshold: float = None
 
+
 @dataclass
-class LeafNodeAttributes:
-    """ attributes class """
-    node_level: int
-    node_name: str
+class LeafNodeAttributes(NodeAttributes):
+    """ leaf attributes class """
     classes: LeafClasses
+
+
+@dataclass
+class DecisionTreeAttributes:
+    """ decision tree attributes class """
+    attributes_map: dict
+    max_depth: int = 20
+    node_purity: float = 0.9
+    max_instances: int = 20
+
+
+def from_str_to_enum(attributes_map: dict) -> dict:
+    """ convert a string into a value of type AttributeType """
+    for name in attributes_map:
+        if attributes_map[name] == "continuous":
+            attributes_map[name] = AttributeType.CONTINUOUS
+        elif attributes_map[name] == "categorical":
+            attributes_map[name] = AttributeType.CATEGORICAL
+        elif attributes_map[name] == "boolean":
+            attributes_map[name] = AttributeType.CATEGORICAL
+        else:
+            raise TypeError("Attribute type not supported")
+    return attributes_map
